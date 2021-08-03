60058615974d46965bf128a4cee7e02372c7c80a
USER <stanmburu254@gmail.com>
2021-08-03 09:49:10 +0300

    first commit

---
+++ src/main/java/net/stanleymburu/employeemanagementbackend/repository/employeeRepository.java
@@ -0,0 +1,10 @@
+package net.stanleymburu.employeemanagementbackend.repository;
+
+import net.stanleymburu.employeemanagementbackend.model.employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface employeeRepository extends JpaRepository<employee, Long> {
+
+}
