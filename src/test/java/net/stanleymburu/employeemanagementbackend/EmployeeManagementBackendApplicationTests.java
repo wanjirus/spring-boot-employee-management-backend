4f5489b951360136ed93a2fd110795c615c03249
USER <stanmburu254@gmail.com>
2021-08-03 08:32:20 +0300

    first commit

---
+++ src/test/java/net/stanleymburu/employeemanagementbackend/EmployeeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.stanleymburu.employeemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
