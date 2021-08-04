0ebbed82674bf923b7dc231a39648b08ad1c4cfd
USER <stanmburu254@gmail.com>
2021-08-04 17:53:11 +0300

    delete employee rest api

--- src/main/java/net/stanleymburu/employeemanagementbackend/controller/employeeController.java
+++ src/main/java/net/stanleymburu/employeemanagementbackend/controller/employeeController.java
@@ -3,12 +3,11 @@ package net.stanleymburu.employeemanagementbackend.controller;
 import net.stanleymburu.employeemanagementbackend.exception.ResourceNotFoundException;
 import net.stanleymburu.employeemanagementbackend.model.employee;
 import net.stanleymburu.employeemanagementbackend.repository.employeeRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.PostUpdate;
+import java.util.HashMap;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -48,6 +47,16 @@ public class employeeController {
         employee.setEmailId(employeeDetails.getEmailId());
         employee updateEmployee=employeeRepository.save(employee);
       return ResponseEntity.ok(updateEmployee);
+    }
+//delete employee rest Api
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HashMap<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        employee employee=employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id :" + id));
+        employeeRepository.delete(employee);
+        HashMap<String, Boolean> response=new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 
 
