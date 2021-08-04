00b85c970726d67c7edfa7430becc15d0e038978
USER <stanmburu254@gmail.com>
2021-08-04 11:41:32 +0300

    creating react get user by id Api

--- src/main/java/net/stanleymburu/employeemanagementbackend/controller/employeeController.java
+++ src/main/java/net/stanleymburu/employeemanagementbackend/controller/employeeController.java
@@ -1,10 +1,14 @@
 package net.stanleymburu.employeemanagementbackend.controller;
 
+import net.stanleymburu.employeemanagementbackend.exception.ResourceNotFoundException;
 import net.stanleymburu.employeemanagementbackend.model.employee;
 import net.stanleymburu.employeemanagementbackend.repository.employeeRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.PostUpdate;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -12,15 +16,32 @@ import java.util.List;
 public class employeeController {
     @Autowired
     private employeeRepository employeeRepository;
+
     //get all employees
     @GetMapping("/employees")
-    public List<employee> getAllEmployees(){
+    public List<employee> getAllEmployees() {
         return employeeRepository.findAll();
+
     }
+
     //create employee rest API
     @PostMapping("/employees")
-    public  employee createEmployee(@RequestBody employee Employee){
+    public employee createEmployee(@RequestBody employee Employee) {
         return employeeRepository.save(Employee);
     }
 
+  //Get Employee By ID
+    @GetMapping("employees/{id}")
+    public ResponseEntity<employee> getEmployeeById(@PathVariable Long id){
+        employee Employee = employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id :" + id));
+    return  ResponseEntity.ok(Employee);
+    }
+
+    
+
+
+
 }
+
+
